e248be4f795dcad928d14aaceed0c9d131e8a417
Martijn Kamphuis <martijn@fam-kamphuis.nl>
2012-11-03 23:37:26 +0100

    Homepage slider + geolocation added

--- _/parts/css_js_includes.php
+++ _/parts/css_js_includes.php
@@ -1,1 +1,5 @@
 <link rel="stylesheet" href="_/stylesheets/screen.css">
+<link rel="stylesheet" href="_/stylesheets/flexslider.css">
+
+<script type="text/javascript" src="_/javascripts/jquery-1.8.2.js"></script>
+<script type="text/javascript" src="_/javascripts/jquery.flexslider.js"></script>
--- index.php
+++ index.php
@@ -8,20 +8,22 @@
 	<body>
 		<div class="wrapper">
 			<?php include '_/parts/header.php' ?>
-			<section id="featured">
-				<article class="slide">
-					<h1>Jerzy Kazemier, triathleet</h1>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempor pharetra quam id fermentum. Cras semper bibendum posuere. Nullam feugiat ipsum libero. Pellentesque sit amet enim non tortor ultricies aliquam. Sed arcu justo, eleifend id condimentum pellentesque, sagittis at nisi. Ut rutrum fringilla ultricies.</p>
-				</article>
-				<article class="slide">
-					<h1>Jerzy Kazemier, triathleet</h1>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempor pharetra quam id fermentum. Cras semper bibendum posuere. Nullam feugiat ipsum libero. Pellentesque sit amet enim non tortor ultricies aliquam. Sed arcu justo, eleifend id condimentum pellentesque, sagittis at nisi. Ut rutrum fringilla ultricies.</p>
-				</article>
-				<article class="slide">
-					<h1>Jerzy Kazemier, triathleet</h1>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempor pharetra quam id fermentum. Cras semper bibendum posuere. Nullam feugiat ipsum libero. Pellentesque sit amet enim non tortor ultricies aliquam. Sed arcu justo, eleifend id condimentum pellentesque, sagittis at nisi. Ut rutrum fringilla ultricies.</p>
-				</article>
-				</section> <!-- section.featured -->
+			<div class="flexslider">
+				<ul id="featured" class="slides">
+					<li class="slide">
+						<h1>Jerzy Kazemier, triathleet</h1>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempor pharetra quam id fermentum. Cras semper bibendum posuere. Nullam feugiat ipsum libero. Pellentesque sit amet enim non tortor ultricies aliquam. Sed arcu justo, eleifend id condimentum pellentesque, sagittis at nisi. Ut rutrum fringilla ultricies.</p>
+					</li>
+					<li class="slide">
+						<h1>Jerzy Kazemier, triathleet</h1>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempor pharetra quam id fermentum. Cras semper bibendum posuere. Nullam feugiat ipsum libero. Pellentesque sit amet enim non tortor ultricies aliquam. Sed arcu justo, eleifend id condimentum pellentesque, sagittis at nisi. Ut rutrum fringilla ultricies.</p>
+					</li>
+					<li class="slide">
+						<h1>Jerzy Kazemier, triathleet</h1>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempor pharetra quam id fermentum. Cras semper bibendum posuere. Nullam feugiat ipsum libero. Pellentesque sit amet enim non tortor ultricies aliquam. Sed arcu justo, eleifend id condimentum pellentesque, sagittis at nisi. Ut rutrum fringilla ultricies.</p>
+					</li>
+				</ul> <!-- section.featured -->
+			</div>
 			<section class="heroes">
 				<h1>Featured Heroes</h1>
 				<article class="hero">
@@ -81,5 +83,31 @@
 			</section> <!-- section.featured -->
 			<?php include '_/parts/footer.php' ?>
 		</div>
+		
+		<script type="text/javascript">
+			$( function() {
+				$('.flexslider').flexslider({
+					animation: "slide",
+					animationLoop: true
+				});
+			});
+			
+			$( function() {
+				// Request a position. We accept positions whose age is not
+				// greater than 10 minutes. If the user agent does not have a
+				// fresh enough cached position object, it will automatically
+				// acquire a new one.
+				var geoposition = navigator.geolocation.getCurrentPosition(successCallback, errorCallback, {maximumAge:600000});
+
+				function successCallback(position) {
+				  // By using the 'maximumAge' option above, the position
+				  // object is guaranteed to be at most 10 minutes old.
+				}
+
+				function errorCallback(error) {
+				  // Update a div element with error.message.
+				}
+			});
+		</script>
 	</body>
 </html>
