c99c00aa314e257ead1343eced2866158010994f
Stef Thoen <stef@baardbaard.nl>
2012-11-03 18:51:53 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Support a Hero</title>
+	</head>
+	<body>
+		<div class="wrapper">
+			<nav>
+				<h1>Logo</h1>
+					<ul>
+						<li>Who Are the Heroes</li>
+						<li>Be a Hero!</li>
+						<form><input class="search" placeholder="Zoek naar helden"></form>
+					</ul>
+				<a href="#" class="account">Hoi Jerzy!</a>
+			</nav>
+			<section id="featured">
+				<article class="slide">
+					<h1>Jerzy Kazemier, triathleet</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempor pharetra quam id fermentum. Cras semper bibendum posuere. Nullam feugiat ipsum libero. Pellentesque sit amet enim non tortor ultricies aliquam. Sed arcu justo, eleifend id condimentum pellentesque, sagittis at nisi. Ut rutrum fringilla ultricies.</p>
+				</article>
+				<article class="slide">
+					<h1>Jerzy Kazemier, triathleet</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempor pharetra quam id fermentum. Cras semper bibendum posuere. Nullam feugiat ipsum libero. Pellentesque sit amet enim non tortor ultricies aliquam. Sed arcu justo, eleifend id condimentum pellentesque, sagittis at nisi. Ut rutrum fringilla ultricies.</p>
+				</article>
+				<article class="slide">
+					<h1>Jerzy Kazemier, triathleet</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempor pharetra quam id fermentum. Cras semper bibendum posuere. Nullam feugiat ipsum libero. Pellentesque sit amet enim non tortor ultricies aliquam. Sed arcu justo, eleifend id condimentum pellentesque, sagittis at nisi. Ut rutrum fringilla ultricies.</p>
+				</article>
+			<section class="heroes">
+				<h2>Featured Heroes</h2>
+				<article class="hero">
+					<a href=""><img src="" alt=""></a>
+					<a href=""><h1 class="heroname"></a>
+					<p class="intro"></p>	
+					<span class="">Locatie</span>
+				</article> <!-- article.hero -->
+			</section> <!-- section.featured -->
+		</div>
+	</body>
+</html>
