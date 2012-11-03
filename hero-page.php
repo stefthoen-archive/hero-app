fd20d9f597d78b9780c72e459fa8d522d6bcd764
Martijn Kamphuis <martijn@fam-kamphuis.nl>
2012-11-03 22:18:43 +0100

    Hero page HTML

---
+++ hero-page.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Support a Hero</title>
+	</head>
+	<body>
+		<div class="wrapper">
+			<?php include '_/parts/header.php' ?>
+			<article>
+				<header>
+					<h1>Jerzy Kazemier, triathleet</h1>
+					<p class="video"></p>
+				</header>
+				<nav class="tabs">
+					<ul><li><a href="#mystory">My story</a></li><li><a href="#updates">Updates</a></li></ul>
+				</nav>
+				<section id="mystory" class="tab">
+					<article>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempor pharetra quam id fermentum. Cras semper bibendum posuere. Nullam feugiat ipsum libero. Pellentesque sit amet enim non tortor ultricies aliquam. Sed arcu justo, eleifend id condimentum pellentesque, sagittis at nisi. Ut rutrum fringilla ultricies.</p>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempor pharetra quam id fermentum. Cras semper bibendum posuere. Nullam feugiat ipsum libero. Pellentesque sit amet enim non tortor ultricies aliquam. Sed arcu justo, eleifend id condimentum pellentesque, sagittis at nisi. Ut rutrum fringilla ultricies.</p>
+					</article>
+				</section>
+				<section id="updates" class="tab">
+					<article class="update">
+						<header>
+							<h1>Update A</h1>
+						</header>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempor pharetra quam id fermentum. Cras semper bibendum posuere. Nullam feugiat ipsum libero. Pellentesque sit amet enim non tortor ultricies aliquam. Sed arcu justo, eleifend id condimentum pellentesque, sagittis at nisi. Ut rutrum fringilla ultricies.</p>
+					</article>
+					<article class="update">
+						<header>
+							<h1>Update B</h1>
+						</header>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempor pharetra quam id fermentum. Cras semper bibendum posuere. Nullam feugiat ipsum libero. Pellentesque sit amet enim non tortor ultricies aliquam. Sed arcu justo, eleifend id condimentum pellentesque, sagittis at nisi. Ut rutrum fringilla ultricies.</p>
+					</article>
+					<article class="update">
+						<header>
+							<h1>Update C</h1>
+						</header>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempor pharetra quam id fermentum. Cras semper bibendum posuere. Nullam feugiat ipsum libero. Pellentesque sit amet enim non tortor ultricies aliquam. Sed arcu justo, eleifend id condimentum pellentesque, sagittis at nisi. Ut rutrum fringilla ultricies.</p>
+					</article>
+				</section>
+				<aside>
+					<dl>
+						<dt>Fans:</dt>
+						<dd>2653</dd>
+						<dt>Total donation:</dt>
+						<dd>&euro;2.535</dd>
+					</dl>
+					<dl>
+						<dt><img src="" alt="&euro;10,-" /></dt>
+						<dd>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</dd>
+						<dt><img src="" alt="Bicycle" /></dt>
+						<dd>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</dd>
+						<dt><img src="" alt="&euro;25,-" /></dt>
+						<dd>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</dd>
+						<dt><img src="" alt="&euro;50,-" /></dt>
+						<dd>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</dd>
+						<dt><img src="" alt="Plane ticket" /></dt>
+						<dd>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</dd>
+					</dl>
+				</aside>
+			</article>
+			<?php include '_/parts/footer.php' ?>
+		</div>
+	</body>
+</html>
